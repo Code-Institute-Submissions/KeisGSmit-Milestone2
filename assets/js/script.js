7944d4cbe12a6fecee0a6249946db7c5813d1e6b
KeisGSmit <kgsmit98@gmail.com>
2020-08-13 09:57:38 +0000

    prevented a user from double clicking a card creating a match

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,11 +3,12 @@
 let cardFlipped = false;
 let firstCard, secondCard; // these 2 variables can vary because it depends on what card you click on, thus they are not declared
 
-let lock = false; // this variable is created to lock the board so that when a user spam clicks, no false matches are made 
+let lock = false; // this variable is created to lock the board so that when a user spam clicks, no false matches are made
 
 //When I click on a card I want it to run a function
 var game = $(".card").click(function () {
-if(lock) return; 
+  if (lock) return;
+  if (this === firstCard) return; // this has been created to prevent matches with a  double click
 
   // the card must first be flipped
   this.classList.add("flip");
@@ -18,7 +19,6 @@ if(lock) return;
     firstCard = this;
   } else {
     //on the second click, a card is flipped and the second card is the card we are currently on
-    cardFlipped = false;
     secondCard = this;
 
     matchCheck();
@@ -39,15 +39,24 @@ function matchCheck() {
 function cardDisable() {
   $(firstCard).off("click");
   $(secondCard).off("click");
+
+  resetBoard();
 }
 
 function unflip() {
-lock=true;
+  lock = true;
 
   setTimeout(() => {
     firstCard.classList.remove("flip");
     secondCard.classList.remove("flip");
 
-    lock = false;
+    resetBoard();
   }, 1500);
+}
+
+function resetBoard() {
+  cardFlipped = false;
+  lock = false;
+  firstCard = null;
+  secondCard = null;
 }
