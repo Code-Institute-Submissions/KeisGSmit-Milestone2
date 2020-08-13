6a77fa004588839188d32f33e182957a8d5de990
KeisGSmit <kgsmit98@gmail.com>
2020-08-13 09:47:31 +0000

    created a lock variable which locks the board to prevent false matches when userss spam click- works fine

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,12 @@
 let cardFlipped = false;
 let firstCard, secondCard; // these 2 variables can vary because it depends on what card you click on, thus they are not declared
 
+let lock = false; // this variable is created to lock the board so that when a user spam clicks, no false matches are made 
+
 //When I click on a card I want it to run a function
 var game = $(".card").click(function () {
+if(lock) return; 
+
   // the card must first be flipped
   this.classList.add("flip");
 
@@ -38,8 +42,12 @@ function cardDisable() {
 }
 
 function unflip() {
+lock=true;
+
   setTimeout(() => {
     firstCard.classList.remove("flip");
     secondCard.classList.remove("flip");
+
+    lock = false;
   }, 1500);
 }
