be519c9c13d40ab7b672cde7657408a41a0781ca
KeisGSmit <kgsmit98@gmail.com>
2020-08-25 11:42:31 +0000

    refactored the comments on script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,64 +1,58 @@
-/**three actions can be taken; the card can be flipped and we pick 2 cards (they can vary) - thus we have 3 variables**/
 let cardFlipped = false;
 let firstCard, secondCard; 
-
-let lock = false; // this variable is created to lock the board so that when a user spam clicks, no false matches are made
+/** this variable is created to lock the board so that when a user spam clicks, no false matches are made*/ 
+let lock = false; 
 
 $(".card").click(function () {
   if (lock) return;// this has been created to prevent matches with a  double click
 
+  /**I add the 'flip' class to the firstCard clicked*/
   if (this === firstCard) return; 
-  // the card must first be flipped
-  this.classList.add("flip");
+  this.classList.add("flip"); 
 
+  /**On first click, a card is flipped */ 
   if (!cardFlipped) {
-    // On first click, a card is flipped and the first card is the card we are currently on
     cardFlipped = true;
     firstCard = this;
   } else {
-    //on the second click, a card is flipped and the second card is the card we are currently on
+    /**on the second click, a different card is flipped*/
     secondCard = this;
-    // this function was created so that a score can be held of how the player is progressing through the game
+    /**the score will increase by 1 and a match will be checked */
     points();
     matchCheck();
-    
   }
 });
 
+/**matchCheck checks if the data-match attribute of the 2 cards are the same */
 function matchCheck() {
-  //We need to check if the cards match
   if (firstCard.dataset.match === secondCard.dataset.match) {
-    // then we need to turn off the click event listener for the first card and the second card
 
+/**This function has been created to to prevent cards from being unflipped whiled other cards are being shown*/
     cardDisable();
     // NEEDS TO PLAY A SOUND LIKE 'TING.WAV'
   } else {
-    //if the cards do not match the cards get flipped back over this has to happen in a certain time period
-    
+/**This function was created to unflip the cards if there has been a flase match*/
     unflip();
   }
 }
 
-//This function has been created to to prevent cards from being unflipped whiled other cards are being shown
 function cardDisable() {
   $(firstCard).off("click");
   $(secondCard).off("click");
-
   resetBoard();
 }
 
-//This function was created to unflip the cards if there has been a flase match
 function unflip() {
   lock = true;
 
   setTimeout(() => {
     firstCard.classList.remove("flip");
     secondCard.classList.remove("flip");
-
+/**This was to lock the board so that no cards can be flipped while other cards are exposed to the user*/
     resetBoard();
   }, 1500);
 }
-//This was to lock the board so that no cards can be flipped while other cards are exposed to the user
+
 function resetBoard() {
   cardFlipped = false;
   lock = false;
@@ -75,13 +69,15 @@ function resetBoard() {
   });
 })();
 
-//This is the function which will increase the score of the player as they play the game 
+
+/**this function was created so that a score can be held of how the player is progressing through the game */ 
 var moves = 0;
 
 function points() {
   moves += 1;
   document.getElementById("score").innerHTML = moves;
 };
+
 /** Now we want to incorporate a high score system
  * when the new game button is clicked, 
  * the score value has to be saved by puttin it in a array 
@@ -91,7 +87,7 @@ function points() {
  * the first 10 items need to be put in individual <li>tags</li> 
  * and be put into <ol id="HighScores"></ol>
  * 
- * const length = array.length < 10 ? array.length : 10;
+const length = array.length < 10 ? array.length : 10;
 let length = array.length;
 if (length > 10) {
     length = 10;
