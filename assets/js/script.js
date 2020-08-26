05d0d69a3d2e7ec9cf27a26a17ff457f5c9cd56a
KeisGSmit <kgsmit98@gmail.com>
2020-08-26 15:21:02 +0000

    new game button is being worked on with 2 functions: endGame and initNewGame. high scores need to be incorportated

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,20 +4,21 @@ let firstCard, secondCard;
 let lock = false; 
 
 $(".card").click(function () {
-  if (lock) return;// this has been created to prevent matches with a  double click
+/**this has been created to prevent matches with a  double click*/  
+    if (lock) return;
 
-  /**I add the 'flip' class to the firstCard clicked*/
+/**add the 'flip' class to the firstCard clicked*/
   if (this === firstCard) return; 
   this.classList.add("flip"); 
 
-  /**On first click, a card is flipped */ 
+/**On first click, a card is flipped */ 
   if (!cardFlipped) {
     cardFlipped = true;
     firstCard = this;
   } else {
-    /**on the second click, a different card is flipped*/
+/**on the second click, a different card is flipped*/
     secondCard = this;
-    /**the score will increase by 1 and a match will be checked */
+/**the score will increase by 1 and a match will be checked */
     points();
     matchCheck();
   }
@@ -27,9 +28,8 @@ $(".card").click(function () {
 function matchCheck() {
   if (firstCard.dataset.match === secondCard.dataset.match) {
 
-/**This function has been created to to prevent cards from being unflipped whiled other cards are being shown*/
+/**This function has been created to prevent cards from being unflipped while other cards are being shown*/
     cardDisable();
-    // NEEDS TO PLAY A SOUND LIKE 'TING.WAV'
   } else {
 /**This function was created to unflip the cards if there has been a flase match*/
     unflip();
@@ -44,15 +44,14 @@ function cardDisable() {
 
 function unflip() {
   lock = true;
-
+/**This was to lock the board so that no cards can be flipped while other cards are exposed to the user*/
   setTimeout(() => {
     firstCard.classList.remove("flip");
     secondCard.classList.remove("flip");
-/**This was to lock the board so that no cards can be flipped while other cards are exposed to the user*/
     resetBoard();
   }, 1500);
 }
-
+/**cards wont be able to be clicked on and won't rotate */
 function resetBoard() {
   cardFlipped = false;
   lock = false;
@@ -78,20 +77,30 @@ function points() {
   document.getElementById("score").innerHTML = moves;
 };
 
-/** Now we want to incorporate a high score system
- * when the new game button is clicked, 
- * the score value has to be saved by puttin it in a array 
- * if the score is < 8 it will not be saved - to prevent 0 being a highscore and incomplete games being counted towards high scores
- * the array has to be ordered in a decreasing order:
- * [small number.....big number]
- * the first 10 items need to be put in individual <li>tags</li> 
- * and be put into <ol id="HighScores"></ol>
- * 
-const length = array.length < 10 ? array.length : 10;
-let length = array.length;
-if (length > 10) {
-    length = 10;
+/** These functions are used to init a new game */
+function initNewGame(){
+    // setup game vars and cards
+let cardFlipped = false;
+let firstCard, secondCard = [undefined,undefined]; 
+let lock = false; 
+var moves = 0;
+document.getElementById("score").innerHTML = moves;
 }
 
-local storage
-*/
+function endGame(){
+    // clean game vars and remove cards
+$('.board > div').removeClass("flip");
+$(firstCard).on("click");
+$(secondCard).on("click");
+(function shuffle() {
+  $(".card").each(function () {
+    let positionShuffle = Math.floor(Math.random() * 18);
+    this.style.order = positionShuffle;
+  });
+})();
+}
+
+function newOrReset(){
+    endGame();
+    initNewGame();
+};
